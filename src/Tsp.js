292f14c5b61af71a9b0f2830dae079902a8a57cd
BoroaDev7 <jmborjas@unitec.edu>
2024-03-17 21:20:01 -0600

    Cambios Tsp sin tiempo

--- src/Tsp.js
+++ src/Tsp.js
@@ -1,13 +1,8 @@
 import React, { useState } from "react";
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Polyline,
-  useMapEvents,
-} from "react-leaflet";
+import { MapContainer, TileLayer, useMapEvents } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import L from "leaflet";
+import L, { DivIcon } from "leaflet";
+import { Marker, Polyline } from "react-leaflet";
 
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
@@ -16,154 +11,165 @@ L.Icon.Default.mergeOptions({
   shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
 });
 
-function LocationMarker() {
-  const [markers, setMarkers] = useState([]);
-  const [tspPath, setTspPath] = useState([]);
-  const [directDistance, setDirectDistance] = useState(0);
-  const [tspDistance, setTspDistance] = useState(0);
+function createIcon(number) {
+  return new DivIcon({
+    html: `<div style="background-color: white; border-radius: 50%; width: 25px; height: 25px; display: flex; justify-content: center; align-items: center;">${number}</div>`,
+    className: "",
+    iconSize: [25, 25],
+  });
+}
 
+function LocationMarker({
+  markers,
+  setMarkers,
+  setInitialPathDistance,
+  tspPath,
+}) {
   useMapEvents({
     click(e) {
       const newMarkers = [...markers, e.latlng];
       setMarkers(newMarkers);
-      updateDistances(newMarkers);
+      calculateInitialPathDistance(newMarkers);
     },
     contextmenu(e) {
       e.originalEvent.preventDefault();
       const newMarkers = markers.slice(0, markers.length - 1);
       setMarkers(newMarkers);
-      updateDistances(newMarkers);
+      calculateInitialPathDistance(newMarkers);
     },
   });
 
-  const updateDistances = (newMarkers) => {
-    calculateDirectDistance(newMarkers);
-    if (newMarkers.length > 2) {
-      calculateTSP(newMarkers);
-    } else {
-      setTspPath([]);
-      setTspDistance(0);
-    }
-  };
-
-  const calculateTSP = (points) => {
-    const distances = points.map((point, i) =>
-      points.map(
-        (otherPoint, j) =>
-          i === j ? Infinity : point.distanceTo(otherPoint) / 1000 // Convertir metros a kilómetros
-      )
-    );
-
-    const included = new Set([0]);
-    const remaining = new Set(points.keys());
-    remaining.delete(0);
-
-    const mstEdges = [];
-
-    while (remaining.size > 0) {
-      let minDistance = Infinity;
-      let minEdge = null;
-
-      for (let i of included) {
-        for (let j of remaining) {
-          if (distances[i][j] < minDistance) {
-            minDistance = distances[i][j];
-            minEdge = { from: i, to: j, distance: minDistance };
-          }
-        }
-      }
-
-      if (minEdge) {
-        mstEdges.push(minEdge);
-        included.add(minEdge.to);
-        remaining.delete(minEdge.to);
-      }
-    }
-
-    const tspPath = [points[0]];
-    let tspDistance = 0;
-
-    while (mstEdges.length > 0) {
-      for (let i = 0; i < mstEdges.length; i++) {
-        const edge = mstEdges[i];
-        if (tspPath.some((point) => points.indexOf(point) === edge.from)) {
-          tspPath.push(points[edge.to]);
-          tspDistance += edge.distance;
-          mstEdges.splice(i, 1);
-          break;
-        }
-      }
-    }
-
-    setTspPath(tspPath.map((point) => [point.lat, point.lng]));
-    setTspDistance(tspDistance);
-  };
-
-  const calculateDirectDistance = (points) => {
-    setDirectDistance(calculateTotalDistance(points));
-  };
-
-  const calculateTotalDistance = (points) => {
-    let totalDistance = 0;
+  const calculateInitialPathDistance = (points) => {
+    let distance = 0;
     for (let i = 0; i < points.length - 1; i++) {
-      totalDistance += points[i].distanceTo(points[i + 1]);
+      distance += points[i].distanceTo(points[i + 1]);
     }
-    return totalDistance / 1000;
-  };
-
-  const resetMap = () => {
-    setMarkers([]);
-    setTspPath([]);
-    setDirectDistance(0);
-    setTspDistance(0);
+    setInitialPathDistance(distance / 1000); // Convert meters to kilometers
   };
 
   return (
     <>
       {markers.map((position, idx) => (
-        <Marker key={idx} position={position}></Marker>
+        <Marker key={idx} position={position} icon={createIcon(idx + 1)} />
       ))}
-      <Polyline positions={markers} color="blue" />
+      <Polyline
+        positions={markers.map((marker) => [marker.lat, marker.lng])}
+        color="blue"
+      />
       <Polyline positions={tspPath} color="red" />
-      <div
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          zIndex: 1000,
-          backgroundColor: "white",
-          padding: "10px",
-        }}
-      >
-        <p>Distancia directa: {directDistance.toFixed(2)} km</p>
-        <p>Distancia TSP: {tspDistance.toFixed(2)} km</p>
-        <button onClick={resetMap}>Resetear Mapa</button>
-      </div>
     </>
   );
 }
 
 function Tsp() {
+  const [markers, setMarkers] = useState([]);
+  const [initialPathDistance, setInitialPathDistance] = useState(0);
+  const [tspPath, setTspPath] = useState([]);
+  const [tspResults, setTspResults] = useState(null);
+
+  const calculateTSP = (points) => {
+    if (points.length < 2) return;
+
+    const distances = points.map((point, i) =>
+      points.map((otherPoint, j) =>
+        i === j ? Infinity : point.distanceTo(otherPoint)
+      )
+    );
+
+    let path = [0];
+    let visited = new Set(path);
+    let totalDistance = 0;
+    let adjacencyList = [];
+
+    while (path.length < points.length) {
+      let last = path[path.length - 1];
+      let nextMinDist = Infinity;
+      let nextIndex = -1;
+
+      distances[last].forEach((dist, index) => {
+        if (dist < nextMinDist && !visited.has(index)) {
+          nextMinDist = dist;
+          nextIndex = index;
+        }
+      });
+
+      if (nextIndex >= 0) {
+        path.push(nextIndex);
+        visited.add(nextIndex);
+        totalDistance += nextMinDist;
+        adjacencyList.push({
+          from: last,
+          to: nextIndex,
+          distance: nextMinDist / 1000,
+        });
+      }
+    }
+
+    if (path.length > 1) {
+      const returnDistance = distances[path[path.length - 1]][0];
+      totalDistance += returnDistance;
+      adjacencyList.push({
+        from: path[path.length - 1],
+        to: 0,
+        distance: returnDistance / 1000,
+      });
+    }
+
+    const latLngPath = path.map((index) => points[index]);
+    latLngPath.push(points[0]);
+    setTspPath(latLngPath.map((point) => [point.lat, point.lng]));
+    setTspResults({ path: adjacencyList, totalDistance: totalDistance / 1000 });
+  };
+
+  const startSimulation = () => {
+    calculateTSP(markers);
+  };
+
+  const resetMap = () => {
+    setMarkers([]);
+    setInitialPathDistance(0);
+    setTspPath([]);
+    setTspResults(null);
+  };
+
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
-      <MapContainer
-        center={[14.1, -86.5]}
-        zoom={7}
-        style={{ height: "800px", width: "80%" }}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          maxZoom={19}
-        />
-        <LocationMarker />
-      </MapContainer>
+    <div style={{ display: "flex", height: "100vh" }}>
+      <div style={{ width: "80%" }}>
+        <MapContainer
+          center={[14.1, -86.5]}
+          zoom={7}
+          style={{ height: "100vh" }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            maxZoom={19}
+          />
+          <LocationMarker
+            markers={markers}
+            setMarkers={setMarkers}
+            setInitialPathDistance={setInitialPathDistance}
+            tspPath={tspPath}
+          />
+        </MapContainer>
+      </div>
+      <div style={{ width: "20%", padding: "10px", overflowY: "auto" }}>
+        <button onClick={startSimulation}>Iniciar simulación</button>
+        <button onClick={resetMap}>Resetear Mapa</button>
+        <p>Distancia total inicial: {initialPathDistance.toFixed(2)} km</p>
+        {tspResults && (
+          <>
+            <h3>Lista de Adyacencia (TSP)</h3>
+            {tspResults.path.map((entry, index) => (
+              <div key={index}>
+                {`De ${entry.from + 1} a ${
+                  entry.to + 1
+                } - Distancia: ${entry.distance.toFixed(2)} km`}
+              </div>
+            ))}
+            <p>Distancia total TSP: {tspResults.totalDistance.toFixed(2)} km</p>
+          </>
+        )}
+      </div>
     </div>
   );
 }
